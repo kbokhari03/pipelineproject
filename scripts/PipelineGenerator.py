e9fd4c383ec1c2c5b854b7298045087a3934cd89
kbokhari03 <kbokhari@luc.edu>
2026-03-02 18:00:22 -0600

    Done with this

--- scripts/PipelineGenerator.py
+++ scripts/PipelineGenerator.py
@@ -1,24 +1,46 @@
 from Bio import SeqIO
-
+import gzip
 
 HCMVgenome = snakemake.input.CodingDomainFile
+numberOfCodingDomains = 0
 
-listIndex = SeqIO.index(HCMVgenome, "fasta")
-numberOfCodingDomains = len(listIndex)
+for record in SeqIO.parse(HCMVgenome, "fasta"):
+    numberOfCodingDomains += 1
 
-pipelineFile = snakemake.ouput.pipeline
+
+pipelineFile = snakemake.output.pipeline
 
 readsBefore = snakemake.input.readBeforeList
 readsAfter = snakemake.input.readsAfterList
 
+searchResults = snakemake.input.searchResultsList
+sampleNames = snakemake.params.samples
+
+
+def countReadsgZ(gZFile):
+    with gzip.open(gZFile, "rt") as f:
+        return sum(1 for line in f) / 4
+
+def countRegularFastQFile(FastQFile):
+    with open(FastQFile, "r") as f:
+        return sum(1 for line in f) / 4
 
 
 
 with open(pipelineFile, "w") as f:
-    f.write(f"The HCMV genome (GCF_000845245.1) has {numberOfCodingDomains}  CDS.")
-    for i in range(len(readsBefore)):
-        beforeIndex = len(SeqIO.index(readsBefore[i],"fasta"))
-        afterIndex = len(SeqIO.index(readsAfter[i],"fasta"))
-        
+    f.write(f"The HCMV genome (GCF_000845245.1) has {numberOfCodingDomains}  CDS.\n")
+    for name, rdBefore, rdAfter, sResults in zip(sampleNames,readsBefore,readsAfter,searchResults):
+        f.write(f"Sample {name} had {countRegularFastQFile(rdBefore)} read pairs before and {countReadsgZ(rdAfter)} read pairs after Bowtie2 filtering\n")
+        f.write(f"{name}:\n")
+        f.write("sacc\tpident\tlength\tqstart\tqend\tsstart\tsend\tbitscore\tevalue\tstitle\n")
+        with open(sResults, "r") as r:
+            for line in r:
+                f.write(line + "\n")
+
+    
+
+
+
+
 
 
